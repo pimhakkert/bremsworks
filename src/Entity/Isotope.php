b00f297eab0ac11f927eee783c1215451f63feee
pimhakkert <pim.hakkert@devoteam.com>
2024-12-29 13:36:01 -0500

    Added the complete isotope entity, which still requires a seeder Added the complete product entity

---
+++ src/Entity/Isotope.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IsotopeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IsotopeRepository::class)]
+class Isotope
+{
+    /*Isotope names*/
+    const AMERICIUM_241 = 'Americium-241';
+    const CESIUM_137 = 'CESIUM-137';
+    const COBALT_60 = 'Cobalt-60';
+    const STRONTIUM_90 = 'Strontium-90';
+    const URANIUM_235 = 'Uranium-235';
+    const URANIUM_238 = 'Uranium-238';
+    const THORIUM_232 = 'Thorium-232';
+
+    /*Isotope shorthands*/
+    const AM_241 = 'Am-241';
+    const CS_137 = 'Cs-137';
+    const CO_60 = 'Co-60';
+    const SR_90 = 'Sr-90';
+    const U_235 = 'U-235';
+    const U_238 = 'U-238';
+    const TH_232 = 'Th-232';
+
+    const ALL_NAMES = [
+        self::AMERICIUM_241,
+        self::CESIUM_137,
+        self::COBALT_60,
+        self::STRONTIUM_90,
+        self::URANIUM_235,
+        self::URANIUM_238,
+        self::THORIUM_232
+    ];
+
+    const ALL_SHORTHANDS = [
+        self::AM_241,
+        self::CS_137,
+        self::CO_60,
+        self::SR_90,
+        self::U_235,
+        self::U_238,
+        self::TH_232
+    ];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shorthand = null;
+
+    /**
+     * @var Collection<int, Product>
+     */
+    #[ORM\ManyToMany(targetEntity: Product::class, inversedBy: 'isotopes')]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        if(!in_array($name, self::ALL_NAMES, true)) {
+            throw new \Exception("Illegal isotope name: " . $name);
+        }
+
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getShorthand(): ?string
+    {
+        return $this->shorthand;
+    }
+
+    public function setShorthand(string $shorthand): static
+    {
+        if(!in_array($shorthand, self::ALL_SHORTHANDS, true)) {
+            throw new \Exception("Illegal isotope shorthand: " . $shorthand);
+        }
+
+        $this->shorthand = $shorthand;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        $this->products->removeElement($product);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    /* Conditions */
+    const CONDITION_NEW_IN_BOX = 'New in box';
+    const CONDITION_NEW = 'New';
+    const CONDITION_REFURBISHED = 'Refurbished';
+    const CONDITION_USED = 'Used';
+    const CONDITION_PARTS = 'Parts';
+
+    const ALL_CONDITIONS = [
+        self::CONDITION_NEW_IN_BOX,
+        self::CONDITION_NEW,
+        self::CONDITION_REFURBISHED,
+        self::CONDITION_USED,
+        self::CONDITION_PARTS
+    ];
+
+    /* Categories */
+    const CATEGORY_MINERALS = 'Minerals';
+    const CATEGORY_SMOKE_ALARMS = 'Smoke alarms';
+    const CATEGORY_DIALS = 'Dials';
+    const CATEGORY_CERAMICS = 'Ceramics';
+    const CATEGORY_ELECTRONIC_COMPONENTS = 'Electronic components';
+    const MISCELLANEOUS = 'Miscellaneous';
+
+    const ALL_CATEGORIES = [
+        self::CATEGORY_MINERALS,
+        self::CATEGORY_SMOKE_ALARMS,
+        self::CATEGORY_DIALS,
+        self::CATEGORY_CERAMICS,
+        self::CATEGORY_ELECTRONIC_COMPONENTS,
+        self::MISCELLANEOUS
+    ];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    private ?User $reservedBy = null;
+
+    #[ORM\Column(length: 255)]
+    private string $title;
+
+    #[ORM\Column(length: 255)]
+    private string $description;
+
+    #[ORM\Column]
+    private int $quantity;
+
+    #[ORM\Column]
+    private int $price;
+
+    #[ORM\Column(length: 255)]
+    private string $category;
+
+    /**
+     * @var Collection<int, Isotope>
+     */
+    #[ORM\ManyToMany(targetEntity: Isotope::class, mappedBy: 'products')]
+    private Collection $isotopes;
+
+    #[ORM\Column]
+    private int $width;
+
+    #[ORM\Column]
+    private int $height;
+
+    #[ORM\Column]
+    private int $depth;
+
+    #[ORM\Column]
+    private int $weight;
+
+    #[ORM\Column(length: 255)]
+    private string $color;
+
+    #[ORM\Column(length: 255)]
+    private string $brand;
+
+    #[ORM\Column(length: 255)]
+    private string $model;
+
+    #[ORM\Column(length: 255)]
+    private string $condition;
+
+    public function __construct()
+    {
+        $this->isotopes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): Product
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getReservedBy(): ?User
+    {
+        return $this->reservedBy;
+    }
+
+    public function setReservedBy(?User $reservedBy): Product
+    {
+        $this->reservedBy = $reservedBy;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): Product
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): Product
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): Product
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): Product
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): Product
+    {
+        if(!in_array($category, self::ALL_CATEGORIES, true)) {
+            throw new \Exception("Illegal product category: " . $category);
+        }
+
+        $this->category = $category;
+        return $this;
+    }
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function setWidth(int $width): Product
+    {
+        $this->width = $width;
+        return $this;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): Product
+    {
+        $this->height = $height;
+        return $this;
+    }
+
+    public function getDepth(): int
+    {
+        return $this->depth;
+    }
+
+    public function setDepth(int $depth): Product
+    {
+        $this->depth = $depth;
+        return $this;
+    }
+
+    public function getWeight(): int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(int $weight): Product
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): Product
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function getBrand(): string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): Product
+    {
+        $this->brand = $brand;
+        return $this;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): Product
+    {
+        $this->model = $model;
+        return $this;
+    }
+
+    public function getCondition(): string
+    {
+        return $this->condition;
+    }
+
+    public function setCondition(string $condition): Product
+    {
+        if(!in_array($condition, self::ALL_CONDITIONS, true)) {
+            throw new \Exception("Illegal product condition: " . $condition);
+        }
+
+        $this->condition = $condition;
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Isotope>
+     */
+    public function getIsotopes(): Collection
+    {
+        return $this->isotopes;
+    }
+
+    public function addIsotope(Isotope $isotope): static
+    {
+        if (!$this->isotopes->contains($isotope)) {
+            $this->isotopes->add($isotope);
+            $isotope->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIsotope(Isotope $isotope): static
+    {
+        if ($this->isotopes->removeElement($isotope)) {
+            $isotope->removeProduct($this);
+        }
+
+        return $this;
+    }
+}
